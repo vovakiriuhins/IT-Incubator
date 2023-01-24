33270a849f768155c0f4f8ed9aae9ce5f2dce684
vovakiriuhins <vova11999977@gmail.com>
2023-01-24 20:26:17 +0300

    04.js
    codewars

---
+++ codewars/04.js
@@ -0,0 +1,8 @@
+function removeNb (n) {
+    var results = [];
+  for (var a = 1; a <= n; a++) {
+    var b = (n * (n + 1) / 2 - a) / (a + 1);
+    if (b % 1 === 0 && b <= n) results.push([a, b]);
+  }
+  return results;
+}
