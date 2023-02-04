e72f390df71171e98b8ce4fc8eb49cb0952f7471
vovakiriuhins <vova11999977@gmail.com>
2023-02-04 17:45:59 +0300

    78.js
    freecodecamp

---
+++ freecodecamp/78.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Vova",
+      "title": "Best",
+      "release_year": 1997,
+      "formats": [
+        "lol",
+        "ghg",
+        "qwerty"
+      ]
+    }
+  ];
